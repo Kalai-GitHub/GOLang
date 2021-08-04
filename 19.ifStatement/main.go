4a96910313ceeefbd8434987e64cb39ebbc6f3d2
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-04 00:05:09 -0700

    if statement

--- 19.ifStatement/main.go
+++ 19.ifStatement/main.go
@@ -23,4 +23,14 @@ func main() {
 		fmt.Println("condition is true")
 	}
 
+	fmt.Println("\nmultiple of 3 and 5= FizzBuzz, multiple of 3 = Fizz, multiple of 5 = Buzz")
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Printf("%d\tFizzBuzz\n", i)
+		} else if i%3 == 0 {
+			fmt.Printf("%d\tFizz\n", i)
+		} else if i%5 == 0 {
+			fmt.Printf("%d\tBuzz\n", i)
+		}
+	}
 }
