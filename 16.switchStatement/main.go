26611bf240e888cfab40e977608d999b76a42597
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-04 00:17:57 -0700

    switch statement example

--- 16.switchStatement/main.go
+++ 16.switchStatement/main.go
@@ -3,19 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Enter a number from 1 to 5")
+	fmt.Println("Enter a number from 1 to 7")
 	var val int
 	fmt.Scanf("%d", &val)
 	switch val {
 	case 1:
 		fmt.Println("you have entered - one")
+		fallthrough // it will execute the next case too
 	case 2:
 		fmt.Println("you have entered - two")
 	case 3:
 		fmt.Println("you have entered - three")
 	case 4:
 		fmt.Println("you have entered - four")
-	case 5:
-		fmt.Println("you have entered - five")
+		fallthrough // it will execute the next case too
+	case 5, 6, 7:
+		fmt.Println("you have entered - five/six/seven")
+	default:
+		fmt.Println("default case")
 	}
 }
