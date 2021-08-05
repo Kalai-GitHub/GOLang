e05174bc7e3c938e003539a1b02b9abcd2b28d83
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-04 21:40:21 -0700

    slice to append the values at the end of the slice

--- 25.slices_append/main.go
+++ 25.slices_append/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Printf("%T\n", slice2)
 
 	s1 := []int{1, 2, 3, 4}
+	s1 = append(s1, 9, 9)
+	fmt.Println("s1", s1)
 	s2 := []int{10, 20, 30}
 	s1 = append(s1, s2...) //adding all the values of s2 in s1
 	fmt.Println("s1", s1)
