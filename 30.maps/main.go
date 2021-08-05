f73aa691fae0b15470b7accbacf3a0d1a75d6838
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-05 13:39:52 -0700

    map declaration types

---
+++ 30.maps/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//map type1
+	x := make(map[string]int)
+	x["H2O"] = 100
+	fmt.Println(x, len(x))
+
+	//map type2
+	m := map[string]string{
+		"A": "Apple",
+		"B": "Ball",
+		"C": "Car",
+	}
+	fmt.Println(m)
+	fmt.Println(m["B"])
+	fmt.Println(m["C"])
+
+	i, ok := m["K"]
+	fmt.Println(i, ok)
+
+	if j, ok := m["B"]; ok {
+		fmt.Println("The value is : ", j)
+	}
+
+	fmt.Println("*************************************")
+
+	el := map[string]map[string]string{
+		"flower": {
+			"name": "rose",
+			"age":  "20",
+		},
+		"gadjets": {
+			"name": "TV",
+			"age":  "10",
+		},
+	}
+	fmt.Println(el)
+
+	if ele, ok := el["gadjets"]; ok {
+		fmt.Println(ele["name"], ele["age"])
+	}
+	if ele, ok := el["flower"]["name"]; ok {
+		fmt.Println(ele)
+	}
+}
