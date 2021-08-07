8d0b21461cd942b6e8e1a48463647a74f76769bf
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-06 18:53:09 -0700

    Accessing struct values in map

---
+++ exercise#22N3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	type person struct {
+		firstName               string
+		lastName                string
+		favoriteIcecreamFlavors []string
+	}
+
+	p1 := person{
+		firstName:               "Kuhan",
+		lastName:                "Thiyagu",
+		favoriteIcecreamFlavors: []string{"Vannila", "strawberry"},
+	}
+
+	p2 := person{
+		firstName:               "Kavin",
+		lastName:                "Thiyagarajan",
+		favoriteIcecreamFlavors: []string{"chocolate", "Pistacho"},
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+
+	for i, v := range p1.favoriteIcecreamFlavors {
+		fmt.Println(i, v)
+	}
+
+	x := map[string]person{
+		p1.lastName: p1,
+		p2.lastName: p2,
+	}
+	fmt.Println(x)
+
+	for i, v := range x {
+		fmt.Println("Index:", i)
+		fmt.Println("Last Name:", v.firstName)
+		for j, v1 := range v.favoriteIcecreamFlavors {
+			fmt.Println(j, v1)
+		}
+	}
+
+}
