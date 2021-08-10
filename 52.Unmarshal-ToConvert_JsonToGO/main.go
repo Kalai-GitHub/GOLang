80d8cbf796fc324759739debd661d553cefbad36
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-09 22:02:10 -0700

    Unmarshal for convert Json to Go struct

---
+++ 52.Unmarshal-ToConvert_JsonToGO/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	First  string `json:"First"` //upper case is for export the property as a public
+	Last   string `json:"Last"`
+	Age    int    `json:"Age"` // this wont display any value due to int value is not passed by the input string.
+	StrAge string `json:"StrAge"`
+}
+
+func main() {
+	p := `[{"First":"Kalai","Last":"murugu","StrAge":"37"},{"First":"Kavin","Last":"Thiyagu","Age":"10"}]`
+	bs := []byte(p)
+	fmt.Printf("%T\n", p)
+	fmt.Printf("%T\n", bs)
+
+	var people []Person
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
+
+	for i, v := range people {
+		fmt.Println("Person Number ", i)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
+}
