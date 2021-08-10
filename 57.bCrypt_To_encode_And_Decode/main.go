8305c136028284e823ca50eb2dfeeca2dba3e19b
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-09 21:31:22 -0700

    bcrypt - given password has encoded using GenerateFromPassword. commpared the password for login using CompareHashAndPassword

---
+++ 57.bCrypt_To_encode_And_Decode/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	pwd := `Password123`
+	bs, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Passwrod", pwd)
+	fmt.Println(bs)
+
+	fmt.Println("--------------------------------------")
+
+	loginPwd := `Password123`
+
+	//Incorrect passwrod
+	//loginPwd := `1212#$$#$&@` // will show the error message
+
+	err = bcrypt.CompareHashAndPassword(bs, []byte(loginPwd))
+
+	if err != nil {
+		fmt.Println("You Can't Login")
+		return
+	}
+
+	fmt.Println("You are logged in")
+}
