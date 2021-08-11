0bd545e9a1646ecb8ce2d2c54e80d5f2153eeda0
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-11 12:10:21 -0700

    channe - send value onto the channel, code does not work

--- 68.ChannelMake_OnlySend_Onto_Channel_codeNotWork/main.go
+++ 68.ChannelMake_OnlySend_Onto_Channel_codeNotWork/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	// c := make(chan<- int, 1) //only to send the value onto the channel
 
-	// c = 33 //send value onto the channel,
+	// c <- 33 //send value onto the channel,
 
 	// fmt.Println(<-c) //invalid operation: cannot receive from send-only channel c (variable of type chan<- int)compilerInvalidReceive
 
