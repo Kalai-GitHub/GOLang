473c26f3dffc4a6973d0d17d46934c4fe02ccadc
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-10 21:12:14 -0700

    Interface - person struct has speak method and human interface has the same speak method

---
+++ exercise#45N5/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func (p *person) speak() {
+	fmt.Println(p.first, p.last, p.age)
+}
+
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func main() {
+	p := person{
+		first: "Kalai",
+		last:  "Murugu",
+		age:   37,
+	}
+
+	p.speak()
+	saySomething(&p) //can pass only value of p
+	//saySomething(p) - this is wrong, can not use p as type human in argument to saySomething()
+
+}
