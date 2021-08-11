b8fcfe2018d7cef568562d30c7f7a3b2ec742336
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-11 12:11:37 -0700

    channel - receive only channel, cannot send value onto the channel, code does not work

--- 69.ChannelMake_ReceiveOnlyChannel/main.go
+++ 69.ChannelMake_ReceiveOnlyChannel/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	// c := make(<-chan int, 1) //receive only channel
 
-	// c = 33 //cant send value onto the channel, if the channel is receive only.
+	// c <- 33 //cant send value onto the channel, if the channel is receive only.
 
 	// fmt.Println(<-c) //invalid operation: cannot receive from send-only channel c (variable of type chan<- int)compilerInvalidReceive
 
