2056e263c1231f5a3d1254228e50810505d4a386
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-11 11:11:54 -0700

    ChannelMake- only to send the value onto the channel

--- 68.ChannelMake_OnlySend_Onto_Channel_codeNotWork/main.go
+++ 68.ChannelMake_OnlySend_Onto_Channel_codeNotWork/main.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	c := make(chan<- int, 1) //only to send the value onto the channel
+	// c := make(chan<- int, 1) //only to send the value onto the channel
 
-	c = 33
+	// c = 33 //send value onto the channel,
 
-	fmt.Println(<-c) //invalid operation: cannot receive from send-only channel c (variable of type chan<- int)compilerInvalidReceive
+	// fmt.Println(<-c) //invalid operation: cannot receive from send-only channel c (variable of type chan<- int)compilerInvalidReceive
 
 }
