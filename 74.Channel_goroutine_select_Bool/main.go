28893f4e06b8eb18aa7c0db3400acf063d342926
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-11 13:58:37 -0700

    channel with bool- goroutine, select, sending and receiving the value using methods and comma OK for the boolean value check

---
+++ 74.Channel_goroutine_select_Bool/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan bool)
+
+	go send(even, odd, quit)
+
+	recieve(even, odd, quit)
+
+	fmt.Println("Exit")
+}
+
+func send(even, odd chan<- int, quit chan<- bool) {
+	for i := 0; i <= 15; i++ {
+		if i%2 == 0 {
+			even <- i
+		} else {
+			odd <- i
+		}
+	}
+	close(quit)
+}
+
+func recieve(even, odd <-chan int, quit <-chan bool) {
+
+	for {
+		select {
+		case v := <-even:
+			fmt.Println("from even channel:", v)
+		case v := <-odd:
+			fmt.Println("from odd channel:", v)
+		case v, ok := <-quit: //comma Ok
+			if !ok {
+				fmt.Println("from comma ok -quit", v, ok)
+				return
+			} else {
+				fmt.Println("from comma ok - quit", v)
+			}
+		}
+	}
+}
