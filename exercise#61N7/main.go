5223363adf2c0b210f2ac9ac1f6a011ab4d1c106
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-08-12 22:25:22 -0700

    custom error checking - using errors.New()

---
+++ exercise#61N7/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		se := errors.New("square root of negative number is invalid")
+		return 0, sqrtError{"11.110 ", "32.12 ", se}
+	}
+	return 42, nil
+}
