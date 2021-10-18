3b9378d5f9ebba4136a1981ce228411dca306140
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2021-10-18 10:26:11 -0700

    closure - scope of the variable

---
+++ github.com/47.closure/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(increment()()) //x value in increment() will be 0 in the starting, returns 1
+	a := increment()
+	fmt.Println(a()) //x value in increment() will be 0 in the starting, returns 1
+	fmt.Println(a()) //x value in increment() will be 2
+	fmt.Println(a()) //x value in increment() will be 3
+
+	b := increment() //x value in increment() will be 0 in the starting, returns 1
+	fmt.Println(b())
+
+	c := scope()
+	fmt.Println("Scope Method: ", c())
+
+}
+
+func increment() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func scope() func() int {
+	outer_var := 2
+	foo := func() int { return outer_var }
+	return foo
+}
+
+//below code wont compile - outer_var is not defined in this scope
+// func another_scope() func() int {
+// 	outer_var := 444
+// 	return foo
+// }
